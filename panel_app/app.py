a272625d474bcf5f6ad7373204e2b0c696aa916e
Patryk Młynarek <p.mlynarek@sunscrapers.com>
2023-09-05 16:01:17 +0200

    Display real data using Panel.

--- panel_app/app.py
+++ panel_app/app.py
@@ -1,41 +1,45 @@
-import json
-from typing import Dict
-
 import hvplot.pandas  # noqa
 import pandas as pd
 import panel as pn
 from holoviews.element import chart
 
-# Load data from file
-with open("trending.json") as f:
-    trending_json_data: Dict = json.load(f)
+from common.constants import GithubTrendingDateRange
+from common.handlers import GithubTrendingHandler
 
-# Prepare data frames
-df_week: pd.DataFrame = pd.DataFrame(trending_json_data["week"])
-df_month: pd.DataFrame = pd.DataFrame(trending_json_data["month"])
+
+@pn.cache(ttl=5 * 60)
+def get_github_trending_chart(date_range: str) -> chart.Bars:
+    df_repos: pd.DataFrame = pd.DataFrame(
+        GithubTrendingHandler(date_range=GithubTrendingDateRange(date_range)).get_json_repos_data(),
+    )
+    df_repos = df_repos.rename(
+        columns={
+            "full_name": "Repository name",
+            "stars_count": "Stars count",
+            "trending_stars_count": "Trending stars count",
+        }
+    )
+
+    chart_repos: chart.Bars = df_repos.hvplot.bar(
+        x="Repository name",
+        y="Trending stars count",
+        xlabel="Full repository name",
+        hover_cols=["Repository name", "Stars count", "Trending stars count"],
+        responsive=True,
+    ).opts(xrotation=90)
+    return chart_repos
+
 
 # Prepare charts
-chart_week: chart.Bars = df_week.hvplot.bar(
-    x="full_name",
-    y="stargazers_count",
-    xlabel="Full repository name",
-    ylabel="Stars count",
-    responsive=True,
-).opts(xrotation=90)
-
-chart_month: chart.Bars = df_month.hvplot.bar(
-    x="full_name",
-    y="stargazers_count",
-    xlabel="Full repository name",
-    ylabel="Stars count",
-    responsive=True,
-).opts(xrotation=90)
-
+chart_repos_daily: chart.Bars = get_github_trending_chart(date_range=GithubTrendingDateRange.daily.value)
+chart_repos_weekly: chart.Bars = get_github_trending_chart(date_range=GithubTrendingDateRange.weekly.value)
+chart_repos_monthly: chart.Bars = get_github_trending_chart(date_range=GithubTrendingDateRange.monthly.value)
 
 # Prepare tabs to allow switching between charts
 tabs: pn.Tabs = pn.Tabs(
-    ("This week", pn.pane.HoloViews(chart_week, linked_axes=False)),
-    ("This month", chart_month),
+    ("Today", pn.pane.HoloViews(chart_repos_daily, linked_axes=False)),
+    ("This week", pn.pane.HoloViews(chart_repos_weekly, linked_axes=False)),
+    ("This month", chart_repos_monthly),
 )
 
 # Render charts
